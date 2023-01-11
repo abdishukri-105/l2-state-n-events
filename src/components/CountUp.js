9a87c7c0fec05dddb61ef306d07dea166ad31011
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-11 12:19:29 +0300

    done

--- src/components/CountUp.js
+++ src/components/CountUp.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 
 function CountUp(){
-
+    
+    const [count, setCount] = useState(0)
+   
     return (
+        
             <div>
                 <center>
-                    <h4 className="time-text">0</h4>
+                    <h4 className="time-text">{count}</h4>
                 </center>
             </div>
     )
